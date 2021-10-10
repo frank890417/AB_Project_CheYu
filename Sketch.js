6c35e35c1f01179c92775db9473bfc1def7dda1e
Wu Che Yu <frank890417@gmail.com>
2021-10-10 14:47:01 -0400

    update all random to hash generator

--- Sketch.js
+++ Sketch.js
@@ -51,55 +51,260 @@ class Random {
   }
 }
 
-let allFeatureList = {
-  RailType: {
-    Sine: 20,
-    Triangle: 5,
-    Square: 5,
-  },
-};
-
 let R = new Random(seed);
 
-features.RailType = R.random_choice_weight(allFeatureList.RailType);
+class Particle {
+  constructor(args) {
+    let def = {
+      p: createVector(0, 0),
+      v: createVector(0, 0),
+      a: createVector(0, 0),
+      r: R.random_num(0, maxParticleR) * R.random_num(0.1, 1) + 1,
+      id: int(R.random_num(0, 1000)),
+      vFriction: R.random_num(0.99, 1),
+      rFactor: R.random_num(0.99, 0.9995),
+      area: null,
+      hasFruit: R.random_num() < 0.5,
+      color: "white",
+      life: R.random_num(0, 2000),
+    };
+    Object.assign(def, args);
+    Object.assign(this, def);
+  }
+  draw() {
+    mainGraphics.push();
+    mainGraphics.translate(this.p);
+    mainGraphics.fill(this.color);
+    mainGraphics.ellipse(0, 0, this.r, this.r);
 
-var colors = "ff6b35-f7c59f-efefd0-004e89-1a659e"
-  .split("-")
-  .map((a) => "#" + a);
+    if (R.random_dec() < 0.02) {
+      let ss = R.random_num(0, 5);
+      mainGraphics.noFill();
+      mainGraphics.strokeWeight(2);
+      mainGraphics.stroke(this.color);
+      mainGraphics.ellipse(0, 0, this.r * ss, this.r * ss);
+    }
+    mainGraphics.pop();
+  }
+  update() {
+    if (R.random_dec() < 0.01 && this.area) {
+      this.color = R.random_choice(this.area.colors);
+    }
+    if (this.area) {
+      // this.p.x+=cos(this.p.x*this.area.sx)/5
+      // this.p.y+=sin(this.p.y*this.area.sy)/5
+      this.v.x += cos((this.p.x * this.area.sx) / 10) / 10;
+      this.v.y += sin((this.p.y * this.area.sy) / 10) / 10;
 
-function setup() {
-  createCanvas(1000, 1000);
-  pixelDensity(2);
-  background(255);
-  // background(0);
+      // this.p.x +=noise(this.p.x*this.area.noiseX)*this.area.noiseXAmp
+      // this.p.y +=noise(this.p.y*this.area.noiseY)*this.area.noiseYAmp
+      // this.v.x+=this.area.gravity.x
+      // this.v.y+=this.area.gravity.y
+      // this.
+    }
+    this.v.mult(this.vFriction);
+
+    this.r *= this.rFactor;
+    this.p.add(this.v);
+
+    this.v.add(this.a);
+  }
+}
+let overAllTexture;
+let maxParticleR = 30;
+let canvasTexture;
+let particleCount = 100;
+let colors = {
+  Paper: "222-666-999-aaa-ccc-eee-fff-080808".split("-").map((a) => "#" + a),
+  Kimono: "fff-f24-fae8eb-f6caca-e4c2c6-cd9fcc-0a014f"
+    .split("-")
+    .map((a) => "#" + a),
+  Sea: "e63946-f1faee-a8dadc-457b9d-1d3557".split("-").map((a) => "#" + a),
+  Confetti: "201e1f-ff4000-faaa8d-feefdd-50b2c0".split("-").map((a) => "#" + a),
+  Vine: "fff-222-545454-69747c-6baa75-84dd63-cbff4d"
+    .split("-")
+    .map((a) => "#" + a),
+  Festival: "12355b-420039-d72638-ffffff-ff570a".split("-").map((a) => "#" + a),
+  Lolipop: "ff499e-d264b6-a480cf-779be7-49b6ff-fff-000"
+    .split("-")
+    .map((a) => "#" + a),
+  Eastern: "333745-e63462-fe5f55-c7efcf-eef5db".split("-").map((a) => "#" + a),
+  Beans: "fb6107-f3de2c-7cb518-5c8001-fbb02d-fff-111"
+    .split("-")
+    .map((a) => "#" + a),
+  MonoChrome: "000-fff-333".split("-").map((a) => "#" + a),
+  Mint: "000-fff-333-00ffbb".split("-").map((a) => "#" + a),
+  Taxi: "000-fff-333-fff719".split("-").map((a) => "#" + a),
+};
+let useColorSet;
+let mainGraphics;
+let areas = [];
+
+//---------
+
+let allFeatureList = {
+  Color: Object.keys(colors).reduce((obj, key) => {
+    obj[key] = 1;
+    return obj;
+  }, {}),
+  // RailType: {
+  //   Sine: 20,
+  //   Triangle: 5,
+  //   Square: 5,
+  // },
+};
+// features.RailType = R.random_choice_weight(allFeatureList.RailType);
+
+//------------------------------
+
+function renderFeatures() {
+  features.Color = R.random_choice_weight(allFeatureList.Color);
+  useColorSet = colors[features.Color];
 }
 
-function draw() {
-  fill(colors[0]);
-  rect(0, 0, width / 2, height / 2);
-  fill(colors[1]);
-  rect(width / 2, 0, width / 2, height / 2);
-  fill(colors[2]);
-  rect(0, height / 2, width / 2, height / 2);
-  fill(colors[3]);
-  rect(width / 2, height / 2, width / 2, height / 2);
-  noStroke();
-  push();
-  stroke(255);
-  translate(width / 2, height / 2);
-  noFill();
-  strokeWeight(3);
-  for (var i = 0; i < 6; i++) {
-    let r = width / 5 + i * 40;
-    let ang = i + frameCount / 100;
-    stroke(255);
-    ellipse(0, 0, r * 2, r * 2);
-    noFill();
-    fill(colors[i % colors.length]);
-    noStroke();
-    ellipse(cos(ang) * r, sin(ang) * r, 40, 40);
-    noFill();
+function preload() {
+  renderFeatures();
+  canvasTexture = loadImage("canvas.jpeg");
+}
+
+function div(x, y, w, h, z) {
+  if (R.random_num() < 0.5 + z / 8 && z > 0) {
+    let ratio = R.random_num(0.2, 0.8);
+    if (R.random_num() < 0.3) {
+      let ww = w * ratio;
+      div(x, y, ww, h, z - 1);
+      div(x + ww, y, w - ww, h, z - 1);
+    } else {
+      let hh = h * ratio;
+      div(x, y, w, hh, z - 1);
+      div(x, y + hh, w, h - hh, z - 1);
+    }
+  } else {
+    let newArea = {
+      x,
+      y,
+      w,
+      h,
+      sx: R.random_num(0, 2),
+      sy: R.random_num(0, 2),
+      noiseX: R.random_num(0, 100),
+      noiseY: R.random_num(0, 1000),
+      noiseXAmp: R.random_num(-5, 5),
+      noiseYAmp: R.random_num(-5, 5),
+      gravity: createVector(R.random_num(-0.1, 0.1), R.random_num(-0.1, 0.1)),
+      colors: useColorSet,
+      id: int(R.random_num(0, 100000)),
+    };
+    mainGraphics.noStroke();
+    areas.push(newArea);
+    mainGraphics.fill(R.random_num(newArea.colors));
+    mainGraphics.rect(x, y, w, h);
   }
+}
+let particles = [];
+let ang;
+function setup() {
+  maxParticleR = R.random_num(25, 80);
+
+  pixelDensity(2);
+  createCanvas(1200, 1200);
+  mainGraphics = createGraphics(width, height);
+  background(100);
+
+  ang = R.random_num(-0.5, 0.5);
+
+  mainGraphics.fill(0);
+  mainGraphics.rect(0, 0, width, height);
+
+  // translate(width/2,height/2)
+  // rotate(random())
+  // translate(-width/2,-height/2)
+
+  overAllTexture = createGraphics(width, height);
+  overAllTexture.loadPixels();
+  for (var i = 0; i < width + 50; i++) {
+    for (var o = 0; o < height + 50; o++) {
+      overAllTexture.set(
+        i,
+        o,
+        color(
+          200,
+          noise(i / 10, (i * o) / 300) * R.random_choice([0, 0, 20, 100])
+        )
+      );
+    }
+  }
+  overAllTexture.updatePixels();
+
+  translate(width / 2, height / 2);
+  rotate(ang);
+  translate(-width / 2, -height / 2);
+
+  div(0, 0, width, height, int(R.random_num(2, 5)));
+  for (var i = 0; i < particleCount; i++) {
+    particles.push(
+      new Particle({
+        p: createVector(width / 2, height / 2),
+        v: createVector(0, 1)
+          .rotate(R.random_num(0, 2 * PI))
+          .mult(5),
+        color: R.random_choice(colors),
+      })
+    );
+  }
+
+  mainGraphics.translate(width / 2, height / 2);
+  mainGraphics.rotate(ang);
+  mainGraphics.translate(-width / 2, -height / 2);
+}
+
+let checkParticleInArea = (area, p) => {
+  return (
+    p.p.x > area.x &&
+    p.p.x < area.x + area.w &&
+    p.p.y > area.y &&
+    p.p.y < area.y + area.h
+  );
+};
+
+function draw() {
+  mainGraphics.noStroke();
+
+  // 	if (mouseIsPressed){
+  // 		particles.push(new Particle({
+  // 			p: createVector(mouseX,mouseY),
+  // 			v: p5.Vector.random2D().mult(5),
+  // 			color: random(colors)
+  // 		}))
+  // 	}
+
+  particles.forEach((particle) => {
+    particle.update();
+    areas.forEach((area) => {
+      if (checkParticleInArea(area, particle)) {
+        if (particle.area !== area) {
+          particle.area = area;
+          particle.color = R.random_choice(area.colors);
+          particle.v.rotate(R.random_num(-1, 1));
+          if (R.random_dec() < 0.1) {
+            let p = new Particle({
+              p: particle.p.copy(),
+              v: particle.v.copy(),
+              a: particle.a.copy(),
+            });
+            particles.push(p);
+          }
+        }
+      }
+    });
+    particle.draw();
+  });
+
+  image(mainGraphics, 0, 0);
+
+  push();
+  blendMode(MULTIPLY);
+  image(canvasTexture, 0, 0, 1920 * 1.15, 1080 * 1.15);
   pop();
 }
 
